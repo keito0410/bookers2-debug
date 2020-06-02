369737d85af34ad6ed9acf4bb0d3cd76b8744374
vagrant <vagrant@localhost.localdomain>
2020-06-02 10:04:42 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,26 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def show
   	@book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
+  end
+
+  def new
+    @book = Book.new
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user = current_user
+    @book.body = params[:book][:body]
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
@@ -39,10 +50,19 @@ class BooksController < ApplicationController
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
+  def ensure_correct_user
+       @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to books_path
+    end
+  end
+
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
+
 
 end
