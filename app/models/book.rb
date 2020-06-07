e48acf84c33abb268b46ccef81be57a5adc90950
vagrant <vagrant@localhost.localdomain>
2020-06-07 08:11:43 +0000

    Finish 応用課題6

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
-class Book < ApplicationRecord
+  class Book < ApplicationRecord
 	belongs_to :user
 	has_many :book_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
@@ -9,4 +9,4 @@ class Book < ApplicationRecord
 	#presence trueは空欄の場合を意味する。
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
-end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :relationships
+  has_many :relationships, foreign_key: 'user_id'
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
